6b959a2f4ea2059226c5690c648c09b33c8e5dce
rickparrish <github@rick.parrish>
2016-10-30 10:31:37 -0400

    Report errors (so if your IP changes and your API key no longer works, you'll know!) Also apparently I forgot to commit the previous change that modified the message content for notices

--- lndcreditwatch/Program.cs
+++ lndcreditwatch/Program.cs
@@ -16,82 +16,75 @@ namespace lndcreditwatch
         static void Main(string[] args)
         {
             MainAsync(args).GetAwaiter().GetResult();
-            if (Debugger.IsAttached) Console.ReadKey();
+            if (Debugger.IsAttached) {
+                Console.WriteLine("Hit a key to quit");
+                Console.ReadKey();
+            }
         }
 
         static async Task MainAsync(string[] args)
         {
             Console.WriteLine(" lndcreditwatch starting up");
 
-            using (LNDynamic client = new LNDynamic(Config.Default.APIID.GetPlainText(), Config.Default.APIKey.GetPlainText()))
-            {
-                // Get current credit balance
-                double CurrentCreditBalance = await client.BillingCreditAsync();
+            try {
+                using (LNDynamic client = new LNDynamic(Config.Default.APIID.GetPlainText(), Config.Default.APIKey.GetPlainText())) {
+                    // Get current credit balance
+                    double CurrentCreditBalance = await client.BillingCreditAsync();
 
-                // Handle the midnight hour, which will alert us to the usage from the previous day
-                string DailyUsageFilename = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "dailyusage.txt");
-                if (DateTime.Now.Hour == 0)
-                {
-                    Console.WriteLine(" midnight hour, alerting previous day's usage");
-                    if (File.Exists(DailyUsageFilename))
-                    {
-                        // Alert daily usage
-                        SendEmail($"current Luna Node credit balance: ${CurrentCreditBalance}\r\nlndcreditwatch daily recap:\r\n\r\n{File.ReadAllText(DailyUsageFilename)}");
+                    // Handle the midnight hour, which will alert us to the usage from the previous day
+                    string DailyUsageFilename = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "dailyusage.txt");
+                    if (DateTime.Now.Hour == 0) {
+                        Console.WriteLine(" midnight hour, alerting previous day's usage");
+                        if (File.Exists(DailyUsageFilename)) {
+                            // Alert daily usage
+                            SendEmail($"current Luna Node credit balance: ${CurrentCreditBalance}\r\nlndcreditwatch daily recap:\r\n\r\n{File.ReadAllText(DailyUsageFilename)}");
 
-                        // And reset
-                        File.Delete(DailyUsageFilename);
+                            // And reset
+                            File.Delete(DailyUsageFilename);
+                        }
                     }
-                }
 
-                // Output some information
-                Console.WriteLine($" fixed cost per interval    : ${Config.Default.FixedCostPerInterval}");
-                Console.WriteLine($" variable cost per interval : ${Config.Default.VariableCostPerInterval}");
-                Console.WriteLine($" total cost per interval    : ${Config.Default.TotalCostPerInterval}");
-                Console.WriteLine($" current credit balance     : ${CurrentCreditBalance}");
+                    // Output some information
+                    Console.WriteLine($" fixed cost per interval    : ${Config.Default.FixedCostPerInterval}");
+                    Console.WriteLine($" variable cost per interval : ${Config.Default.VariableCostPerInterval}");
+                    Console.WriteLine($" total cost per interval    : ${Config.Default.TotalCostPerInterval}");
+                    Console.WriteLine($" current credit balance     : ${CurrentCreditBalance}");
 
-                // Compare to previous credit balance
-                if (Config.Default.PreviousCreditBalance > double.MinValue)
-                {
-                    Console.WriteLine($" previous credit balance    : ${Config.Default.PreviousCreditBalance}");
+                    // Compare to previous credit balance
+                    if (Config.Default.PreviousCreditBalance > double.MinValue) {
+                        Console.WriteLine($" previous credit balance    : ${Config.Default.PreviousCreditBalance}");
 
-                    double CostForThisInterval = Config.Default.PreviousCreditBalance - CurrentCreditBalance;
-                    Console.WriteLine($" cost for this interval     : ${CostForThisInterval}");
+                        double CostForThisInterval = Config.Default.PreviousCreditBalance - CurrentCreditBalance;
+                        double CostPerDay = CostForThisInterval * 24.0; // TODOX Assumes hourly interval
+                        double CostPerMonth = CostPerDay * 365.0 / 12.0;
 
-                    if (CostForThisInterval > Config.Default.TotalCostPerInterval)
-                    {
-                        // We spent too much during this last interval TODO Calculations correct?
-                        double TotalOverspendAmount = CostForThisInterval - Config.Default.TotalCostPerInterval;
-                        double TotalOverspendPercent = TotalOverspendAmount / Config.Default.TotalCostPerInterval;
-                        double VariableOverspendPercent = TotalOverspendAmount / Config.Default.VariableCostPerInterval;
-                        Console.WriteLine($" OVERSPEND by ${TotalOverspendAmount}");
-                        Console.WriteLine($" in other words, {VariableOverspendPercent:P} more was spent on variable costs ({TotalOverspendPercent:P} overall)");
-                        File.AppendAllText(DailyUsageFilename, $"OVERSPEND ${TotalOverspendAmount} {VariableOverspendPercent:P} {TotalOverspendPercent:P}\r\n");
-                        SendEmail($"lndcreditwatch noticed you overspent by ${TotalOverspendAmount} ({VariableOverspendPercent:P} more was spent on variable costs, or {TotalOverspendPercent:P} overall)");
+                        Console.WriteLine($" cost for this interval     : ${CostForThisInterval}");
+                        Console.WriteLine($" cost per day               : ${CostPerDay}");
+                        Console.WriteLine($" cost per month             : ${CostPerMonth}");
+
+                        if (CostForThisInterval > Config.Default.TotalCostPerInterval) {
+                            // We spent too much during this last interval
+                            File.AppendAllText(DailyUsageFilename, $"OVERSPEND\t${CostForThisInterval}\t{CostPerDay}\t{CostPerMonth}\r\n");
+                            SendEmail($"lndcreditwatch noticed you overspent.  At ${CostForThisInterval}/hr you'd be spending:\r\n${CostPerDay}/day\r\n${CostPerMonth}/month.\r\nCurrent balance: ${CurrentCreditBalance}");
+                        } else {
+                            // We were within our spending limit
+                            File.AppendAllText(DailyUsageFilename, $"UNDERSPEND\t${CostForThisInterval}\t{CostPerDay}\t{CostPerMonth}\r\n");
+                            if (Debugger.IsAttached) SendEmail($"lndcreditwatch noticed you underspent.  At ${CostForThisInterval}/hr you'd be spending:\r\n${CostPerDay}/day\r\n${CostPerMonth}/month.\r\nCurrent balance: ${CurrentCreditBalance}");
+                        }
+                    } else {
+                        // No previous balance, so send an email alert saying watch has been setup
+                        Console.WriteLine(" previous credit balance     : N/A (looks like this is our first run)");
+                        SendEmail("lndcreditwatch is now monitoring your account!");
                     }
-                    else
-                    {
-                        // We were within our spending limit TODO Calculations correct?
-                        double TotalUnderspendAmount = Config.Default.TotalCostPerInterval - CostForThisInterval;
-                        double TotalUnderspendPercent = TotalUnderspendAmount / Config.Default.TotalCostPerInterval;
-                        double VariableUnderspendPercent = TotalUnderspendAmount / Config.Default.VariableCostPerInterval;
-                        Console.WriteLine($" UNDERSPEND by ${TotalUnderspendAmount}");
-                        Console.WriteLine($" in other words, {VariableUnderspendPercent:P} less was spent on variable costs ({TotalUnderspendPercent:P} overall)");
-                        File.AppendAllText(DailyUsageFilename, $"UNDERSPEND ${TotalUnderspendAmount} {VariableUnderspendPercent:P} {TotalUnderspendPercent:P}\r\n");
-                        if (Debugger.IsAttached) SendEmail($"lndcreditwatch noticed you underspent by ${TotalUnderspendAmount} ({VariableUnderspendPercent:P} less was spent on variable costs, or {TotalUnderspendPercent:P} overall)");
-                    }
-                }
-                else
-                {
-                    // No previous balance, so send an email alert saying watch has been setup
-                    Console.WriteLine(" previous credit balance     : N/A (looks like this is our first run)");
-                    SendEmail("lndcreditwatch is now monitoring your account!");
-                }
 
-                // Store current credit as previous credit
-                Config.Default.PreviousCreditBalance = CurrentCreditBalance;
-                Config.Default.Save();
+                    // Store current credit as previous credit
+                    Config.Default.PreviousCreditBalance = CurrentCreditBalance;
+                    Config.Default.Save();
 
-                Console.WriteLine(" done");
+                    Console.WriteLine(" done");
+                }
+            } catch (Exception ex) {
+                SendEmail($"Exception running lndcreditwatch: {ex.ToString()}");
             }
         }
 
